90ecad6a6093c4fd4ef42a6f9333638c4db7498a
Maciej N. <magnax@gmail.com>
2026-02-11 21:21:52 +0100

    Add auxins, show auxins, calculate & show affected veins

--- main_dsl.rb
+++ main_dsl.rb
@@ -1,4 +1,5 @@
 require 'raylib/dsl'
+require 'raylib/raymath'
 require 'pry'
 
 #
@@ -20,16 +21,27 @@ class Vein
   end
 end
 
+class Auxin
+  attr_reader :position, :radius, :color
+
+  def initialize(position)
+    @position = position
+    @radius = 8
+    @color = RED
+  end
+end
+
 PHASE = [
-  'Initial state',
-  'Discover veins affected'
+  {key: :init, desc: 'Initial state'},
+  {key: :pull, desc: 'Discover veins affected'}
 ]
 PHASES = PHASE.cycle
+AUXIN_CREATION_RATE = 3
 
 phase = PHASES.next
 
 def show_info(phase, top)
-  draw_text("#{PHASE.index(phase) + 1}. #{phase}.", 20, top, 21, LIGHTGRAY)
+  draw_text("#{PHASE.index(phase) + 1}. [#{phase[:key].to_s}] - #{phase[:desc]}.", 20, top, 21, LIGHTGRAY)
 end
 
 def show_veins
@@ -39,7 +51,23 @@ def show_veins
   end
 end
 
+def add_auxins(w, h)
+  (1..AUXIN_CREATION_RATE).each do
+    position = Raylib::Vector2.create(rand(w), rand(h))
+
+    auxin = Auxin.new(position)
+    @auxins.push(auxin)
+  end
+end
+
+def show_auxins
+  @auxins.each do |v|
+    draw_circle(v.position.x, v.position.y, v.radius, v.color)
+  end
+end
+
 @veins = []
+@auxins = []
 
 init_window(1200, 900, 'Leaf venation patterns demo')
 w = get_screen_width
@@ -50,6 +78,8 @@ position = Raylib::Vector2.create(w/2, h/4*3)
 vein = Vein.new(position)
 @veins.push(vein)
 
+add_auxins(w, h)
+
 until window_should_close
   if is_key_pressed(KEY_SPACE)
     phase = PHASES.next
@@ -58,6 +88,19 @@ until window_should_close
   begin_drawing
   clear_background(get_color(0x181818FF))
   show_veins
+  show_auxins
+  case phase[:key]
+  when :pull
+    @auxins.each do |auxin|
+      closest_vein = @veins.first
+      @veins.each do |vein|
+        if vector2_distance(vein.position, auxin.position) < vector2_distance(closest_vein.position, auxin.position)
+          closest_vein = vein
+        end
+        draw_line_v(closest_vein.position, auxin.position, WHITE)
+      end
+    end
+  end
   show_info(phase, h - 30)
   end_drawing
 end
