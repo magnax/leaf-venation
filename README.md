02d49e8f3f8cf86821fa1c3e34b24eaf2f63438a
Maciej N. <magnax@gmail.com>
2026-02-11 15:25:43 +0100

    Cycle through venation phases

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-`gem install raylib --pre`
-
-Run:
-
-There is executable script which you can just run:
-`./run`
+Modeling and visualization of leaf venation patterns according to algorithm described in [https://dl.acm.org/doi/epdf/10.1145/1073204.1073251].
--- main_dsl.rb
+++ main_dsl.rb
@@ -1,11 +1,30 @@
 require 'raylib/dsl'
+require 'pry'
 
-init_window(800, 450, 'Raylib template - DSL version!')
+PHASE = [
+  'Initial state',
+  'Discover veins affected'
+]
+PHASES = PHASE.cycle
+
+phase = PHASES.first
+
+def show_info(phase, top)
+  draw_text("#{PHASE.index(phase) + 1}. #{phase}.", 20, top, 21, LIGHTGRAY)
+end
+
+init_window(1200, 900, 'Leaf venation patterns demo')
+w = get_screen_width
+h = get_screen_height
 
 until window_should_close
+  if is_key_pressed(KEY_SPACE)
+    phase = PHASES.next
+  end
+  
   begin_drawing
-  clear_background(WHITE)
-  draw_text("You\'re running raylib in Ruby!\nESC to close this window", 190, 200, 20, BLACK)
+  clear_background(get_color(0x181818FF))
+  show_info(phase, h - 30)
   end_drawing
 end
 
