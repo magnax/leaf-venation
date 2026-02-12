ab50250a4cf82eb06086677ede6c0ec5c412c690
Maciej N. <magnax@gmail.com>
2026-02-12 22:09:14 +0100

    Populate new auxin sources, check for proximity

--- main_dsl.rb
+++ main_dsl.rb
@@ -61,12 +61,18 @@ PHASE = [
   {key: :pull, desc: 'Discover veins affected'},
   {key: :pull_normalized, desc: 'Normalize vectors to auxin\'s closest vein'},
   {key: :add, desc: 'Add all vectors to auxin\'s closest vein'},
-  {key: :grow, desc: 'Grow new vein'}
+  {key: :grow, desc: 'Grow new vein'},
+  {key: :populate, desc: 'Populate new auxin sources'}
 ]
 PHASES = PHASE.cycle
 AUXIN_CREATION_RATE = 3
+AUXIMITY = 50
+VEIN_PROXIMITY = 50
 
 phase = PHASES.next
+@populated = false
+@show_auximity = false
+@show_vein_proximity = false
 
 def show_info(phase, top)
   draw_text("#{PHASE.index(phase) + 1}. [#{phase[:key].to_s}] - #{phase[:desc]}.", 20, top, 21, LIGHTGRAY)
@@ -76,13 +82,14 @@ def show_veins
   @veins.each do |v|
     draw_circle(v.position.x, v.position.y, v.radius, v.color)
     draw_circle(v.position.x, v.position.y, v.inner_radius, v.inner_color)
+    draw_circle_lines(v.position.x, v.position.y, VEIN_PROXIMITY, get_color(0x2222ffff)) if @show_vein_proximity
   end
 end
 
 def add_auxins(w, h)
   (1..AUXIN_CREATION_RATE).each do
     position = Raylib::Vector2.create(rand(w), rand(h))
-
+    
     auxin = Auxin.new(position)
     @auxins.push(auxin)
   end
@@ -91,6 +98,7 @@ end
 def show_auxins
   @auxins.each do |v|
     draw_circle(v.position.x, v.position.y, v.radius, v.color)
+    draw_circle_lines(v.position.x, v.position.y, AUXIMITY, get_color(0xff2222ff)) if @show_auximity
   end
 end
 
@@ -134,13 +142,50 @@ end
 
 def grow_new_vein
   @veins.each do |vein|
+    auxins_to_remove = []
     new_growth_pos = vein.fetch_new_growth_position
+    
     if new_growth_pos
       new_vein = Vein.new(new_growth_pos)
       @veins.push(new_vein)
       vein.clear_growth_position
+
+      @auxins.each do |auxin|
+        if vector2_distance(auxin.position, new_vein.position) <= VEIN_PROXIMITY
+          auxins_to_remove << @auxins.index(auxin)
+        end
+      end
     end
+    
+    @auxins = @auxins.reject { |auxin| auxins_to_remove.include?(@auxins.index(auxin)) }
   end
+end
+
+def populate_new_auxins(w, h)
+  n = 0
+  while n < AUXIN_CREATION_RATE
+    position = Raylib::Vector2.create(rand(w), rand(h))
+
+    correct_auxin_position = true
+    @auxins.each do |auxin|
+      if vector2_distance(position, auxin.position) <= AUXIMITY
+        correct_auxin_position = false
+      end
+    end
+
+    @veins.each do |vein|
+      if vector2_distance(position, vein.position) <= VEIN_PROXIMITY
+        correct_auxin_position = false
+      end
+    end
+
+    if correct_auxin_position
+      auxin = Auxin.new(position)
+      @auxins.push(auxin)
+      n +=1
+    end
+  end 
+  @populated = true
 end
 
 @veins = []
@@ -159,7 +204,16 @@ add_auxins(w, h)
 
 until window_should_close
   if is_key_pressed(KEY_SPACE)
+    @populated = false
     phase = PHASES.next
+  end
+
+  if is_key_pressed(KEY_A)
+    @show_auximity = !@show_auximity
+  end
+
+  if is_key_pressed(KEY_V)
+    @show_vein_proximity = !@show_vein_proximity
   end
 
   begin_drawing
@@ -175,6 +229,8 @@ until window_should_close
     add_and_normalize_influences
   when :grow
     grow_new_vein
+  when :populate
+    populate_new_auxins(w, h) unless @populated
   end
   show_info(phase, h - 30)
   end_drawing
